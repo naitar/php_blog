4ba1128318b7dde1bc8b1803218c1ca8014bbf56
naitar <naitaryu@gmail.com>
2021-01-16 08:50:17 +0630

    BLOG

---
+++ blog/index.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>AdminLTE 3 | Widgets</title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+    <!-- Google Font: Source Sans Pro -->
+    <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <section class="content-header">
+
+            <div class="container-fluid ">
+                <div class="row mb-3">
+                    <div class="col-md-12" style="text-align:center">
+                        <h3>Blog SITE</h3>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+        <section class="content">          
+                            
+                <div class="row">
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+
+                    <!-- blogbox -->
+                    <div class="col-md-4">                      
+                            <div class="card card-widget">
+                                <div class="card-header">
+                                    <div style="text-align: center;float:none;">
+                                        <h4>BLog Title</h4>
+                                    </div>
+                                </div>
+                                <div class="card-body">
+                                    <img class="img-fluid pad" src="../dist/img/photo2.png" alt="Photo">
+                                </div>
+                            </div>                     
+                    </div>
+                </div>
+         
+
+
+
+
+
+        </section>
+
+        <footer class="main-footer" style="margin-left:0px !important;">
+            <div class="float-right ">
+              <b>Version</b> 4
+            </div>
+            <strong>Copyright &copy; 2020-2021 <a href="#">Blog </a>.</strong> All rights
+            reserved.
+          </footer>
+
+    </div>
+
+
+
+
+
+    <!-- jQuery -->
+    <script src="../plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../dist/js/adminlte.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../dist/js/demo.js"></script>
+
+</body>
+
+</html>
