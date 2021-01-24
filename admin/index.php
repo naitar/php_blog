320c032d6b47a5b482dc59586cb62c8b86440589
naitar <naitaryu@gmail.com>
2021-01-24 19:40:47 +0630

    fixed

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 require '../Config/config.php';
+require '../Config/common.php';
 
 if (empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
   header('location:login.php');
--- blog/index.php
+++ blog/index.php
@@ -2,6 +2,7 @@
 session_start();
 
 require '../Config/config.php';
+require '../Config/common.php';
 
 if (empty($_SESSION['user_id']) && empty($_SESSION['logged_in'])) {
     header('location:login.php');
